97ee54866939aafd46023037d9c7f5f0cbac7bc6
hrhua <951414416@qq.com>
2018-07-29 11:53:19 +0800

    新建工程hrassistant

---
+++ src/main/java/com/bingo/hrassistant/HrassistantApplication.java
@@ -0,0 +1,12 @@
+package com.bingo.hrassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrassistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrassistantApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bingo/hrassistant/HrassistantApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bingo.hrassistant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HrassistantApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
